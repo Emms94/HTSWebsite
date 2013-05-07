5a97d4e66cc0ff4b8f2560137c240fae9e598ed9
unknown <ts10232@D10232.skolan.hassleholm.se>
2013-05-07 12:33:26 +0200

    kommentar

--- MinHemsida/login.php
+++ MinHemsida/login.php
@@ -1,14 +1,14 @@
 ﻿<!-- Kom ihåg att logga in till databasen först! -->
 <?php
-	include 'C:\Users\ts10232\Desktop\Databashemsida\HTSWebsite\MinHemsida';
+	include 'connect.php';
 	
-         $query = "SELECT name FROM user WHERE name='$_POST[myUser]' AND password='$_POST[myPassword]'";
+         $query = "SELECT namn FROM inloggning WHERE namn='$_POST[myUser]' AND password='$_POST[myPassword]'";
          //echo '<em> ' . $query . ' </em>';
          $result = mysql_query($query);
 		 session_start();
 		 session_unset();
          if (mysql_numrows($result) == 1) {
-		      $_SESSION['session_user']=$_POST[myUser];
+		      $_SESSION['session_user']=$_POST['myUser'];
 			  header('Location: index.html.html');
 		 }
 		 else {
